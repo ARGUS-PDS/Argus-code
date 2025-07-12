739f8eef112ccec35fd6644a591834b2c12fab3e
GustavoEmiliano <gustavoemiliano2014@gmail.com>
2025-07-12 10:04:55 -0300

    feat: implementação toggle de visibilidade para a senha na tela de Login.

--- public/js/Login.js
+++ public/js/Login.js
@@ -21,3 +21,15 @@ document.getElementById('whatsapp').addEventListener('input', function (e) {
   }
   e.target.value = value;
 });
+
+function togglePassword(inputId, iconElement) {
+  const passwordInput = document.getElementById(inputId);
+  
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    iconElement.classList.replace('fa-eye', 'fa-eye-slash');
+  } else {
+    passwordInput.type = "password";
+    iconElement.classList.replace('fa-eye-slash', 'fa-eye');
+  }
+}
